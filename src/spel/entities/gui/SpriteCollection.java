3c885d2e239933606809e44f571dd9b36756ddcb
Tristan Axelsson Smith <tristan23@live.se>
2014-11-03 15:43:33 +0100

    Added Sprites

--- src/spel/entities/gui/SpriteCollection.java
+++ src/spel/entities/gui/SpriteCollection.java
@@ -11,4 +11,9 @@ public class SpriteCollection {
 	public static final Sprite player = new Sprite("res/pixel.png");
 	public static final Sprite menubg = new Sprite("res/sstartsida.png");
 	public static final Sprite pauseCover = new Sprite("res/pausecover.png");
+	public static final Sprite settings = new Sprite("res/settings.png");
+	public static final Sprite quit = new Sprite("res/quit.png");
+	public static final Sprite cont = new Sprite("res/continue.png");
+	
 }
+
