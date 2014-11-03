731a8d14acca30757a638eb2996742a14b36ad22
kalasnokov <gustaffagerstrom@gmail.com>
2014-11-03 13:55:14 +0100

    Shiggy diggy

--- src/spel/entities/Structure.java
+++ src/spel/entities/Structure.java
@@ -2,20 +2,8 @@ package spel.entities;
 
 public class Structure extends Entity {
 
-	public Structure(double xpos, double ypos, String[] frames) {
-		super(xpos, ypos, frames);
+	public Structure(double xpos, double ypos, double height, double width) {
+		super(xpos, ypos, height, width);
 		
 	}
-
-	public Structure(double xpos, double ypos, double height, double width,
-			String filepath) {
-		super(xpos, ypos, height, width, filepath);
-		
-	}
-
-	public Structure(double xpos, double ypos, String filepath) {
-		super(xpos, ypos, filepath);
-		
-	}
-
 }
