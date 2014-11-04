488ca38e0c492f4722805a223fa1ed9b7a6b964a
kalasnokov <gustaffagerstrom@gmail.com>
2014-11-04 18:37:45 +0100

    constructor fix on some structures

--- src/spel/entities/structures/Barn.java
+++ src/spel/entities/structures/Barn.java
@@ -3,8 +3,8 @@ package spel.entities.structures;
 public class Barn extends Structure {
 
 	public Barn(double xpos, double ypos, double height, double width,
-			boolean permanent) {
-		super(xpos, ypos, height, width, permanent);
+			boolean permanent, boolean gridlocked) {
+		super(xpos, ypos, height, width, permanent, gridlocked);
 		// TODO Auto-generated constructor stub
 	}
 
--- src/spel/entities/structures/House.java
+++ src/spel/entities/structures/House.java
@@ -1,11 +1,10 @@
 package spel.entities.structures;
 
-
 public class House extends Structure {
 
 	public House(double xpos, double ypos, double height, double width,
-			boolean permanent) {
-		super(xpos, ypos, height, width, permanent);
+			boolean permanent, boolean gridlocked) {
+		super(xpos, ypos, height, width, permanent, gridlocked);
 		
 	}
 }
